59e9e2a1525f8fe3b2b7165d267eae8ca3330d9a
Ramez <ramezzormati1998@gmail.com>
2020-12-24 21:39:38 +0100

    js update

--- js/verifInscri.js
+++ js/verifInscri.js
@@ -1,35 +1,118 @@
+function verifNomPrenom(ch)
+{
+	var i=0;
+	var ok=0;
+	while(i<ch.length)
+	{
+		if(ch[i].toUpperCase()<'A'||ch[i].toUpperCase()>'Z')
+			return false ; 
+		i++;
+	};
+	return true;
+}
+function increaseHeight()
+{
+	x = parseInt(document.getElementById("bigbox").style.height)+50;
+	document.getElementById("bigbox").style.height = x.toString()+"px";
+	x = parseInt(document.getElementById("hei").style.height)+50;
+	document.getElementById("hei").style.height = x.toString()+"px";
+}
+function decreaseHeight()
+{
+	x = parseInt(document.getElementById("bigbox").style.height)-50;
+	document.getElementById("bigbox").style.height = x.toString()+"px";
+	x = parseInt(document.getElementById("hei").style.height)-50;
+	document.getElementById("hei").style.height = x.toString()+"px";
+}
+
+function VerifNom()
+{
+	var nom=document.getElementById("nom").value;
+	if((!verifNomPrenom(nom)||nom.length==0)&&(document.getElementById("ernom").classList.contains("d-none")==true))
+	{
+		increaseHeight();
+		document.getElementById("ernom").classList.remove('d-none');
+		
+	}else if((verifNomPrenom(nom)&&nom.length>0)&&(document.getElementById("ernom").classList.contains("d-none")==false)){
+		decreaseHeight();
+		document.getElementById("ernom").classList.add('d-none');
+	}
+}
+function VerifPren()
+{
+	var prenom=document.getElementById("prenom").value;
+	if((!verifNomPrenom(prenom)||prenom.length==0)&&(document.getElementById("erpren").classList.contains("d-none")==true))
+	{
+		increaseHeight();
+		document.getElementById("erpren").classList.remove('d-none');
+	
+	}else if((verifNomPrenom(prenom)&&prenom.length>0)&&(document.getElementById("erpren").classList.contains("d-none")==false)){
+		decreaseHeight();
+		document.getElementById("erpren").classList.add('d-none');
+	}
+}
+
+
+function VerifMdp()
+{
+	var mdp=document.getElementById("mdp").value;
+	var mdp2=document.getElementById("mdp2").value;
+	if((mdp!=mdp2)&&(document.getElementById("ermdp").classList.contains("d-none")==true))
+	{
+		increaseHeight();
+		document.getElementById("ermdp").classList.remove('d-none');
+	}else if((mdp==mdp2)&&(document.getElementById("ermdp").classList.contains("d-none")==false))
+	{
+		decreaseHeight();
+		document.getElementById("ermdp").classList.add('d-none');
+	}
+}
+
+
+function disableSubmit()
+{
+	document.getElementById("sub").disabled = true ; 
+	document.getElementById("sub").style.backgroundColor = "grey";
+	document.getElementById("sub").style.border = "grey";
+}
+function enableSubmit()
+{
+	document.getElementById("sub").disabled = false ; 
+	document.getElementById("sub").style.backgroundColor = "#BE3144";
+	document.getElementById("sub").style.border = "#BE3144";
+}
+
 function Verifier()
 {	
-	var c='@';var i;
-	ok=0;
-	
-	var email=document.getElementById("email").value;
 	var mdp=document.getElementById("mdp").value;
 	var mdp2=document.getElementById("mdp2").value;
 	var nom=document.getElementById("nom").value;
 	var prenom=document.getElementById("prenom").value;
 	var gen=document.frm.genre;
-	var add=document.getElementById("adr").value;
-	
-	if(email.length==0 || mdp.length==0||mdp2.length==0||nom.length==0||prenom.length==0||(gen[0].checked==false && gen[1].checked==false)||add.length==0)
-		alert("remplir tous les champ SVP");
-	else{
-		if((email.indexOf(c)==-1)||(email.lastIndexOf(c)!=email.indexOf(c))||email.indexOf(c)>email.lastIndexOf('.')||mail.indexOf('.')==-1)
-			alert("Verifier votre email");
-		else
-		{
-			//alert("remplir tous les champcccccccc SVP");
-		/*
-			for(i=0;i<nom.length;i++)
-				if(toUpperCase(nom[i])<'A'||toUpperCase(nom[i])>'Z')
-					ok++;
-			if(ok>0)*/
-				
-		}
-			
-	}
-	
-		
-	
 
+//verification nom et prenom 
+	if(!verifNomPrenom(nom))
+	{
+		disableSubmit();
+		return false;
+	}
+		
+	if(!verifNomPrenom(prenom))
+	{
+		disableSubmit();
+		return false;
+	}	
+//verif mdp
+	if(mdp!=mdp2)
+	{
+		disableSubmit();
+		return false;
+	}
+//verif radio 
+	if(gen[0].checked==false&&gen[1].checked==false)
+	{
+		disableSubmit();
+		return false;
+	}
+	enableSubmit();
 }
