a39399c0736d33ae1e6b5b368628a55864d97c75
Houssem Sakli <sakli.houssem@yahoo.com>
2020-12-28 20:19:46 +0100

    Header fixes

--- php/articlesenfant.php
+++ php/articlesenfant.php
@@ -81,10 +81,10 @@
                 }else {  
                 ?>
                             <ul class="navbar-nav col-md-9">
-                                <li class="nav-item active">
+                                <li class="nav-item">
                                     <a class="nav-link" href="../php/index.php">Accueil <span class="sr-only">(current)</span></a>
                                 </li>
-                                <li class="nav-item dropdown">
+                                <li class="nav-item dropdown active">
                                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     Articles
                                     </a>
